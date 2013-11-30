fcba06364f6498ca29cc26196a5a77926eda3fdc
ks <ks@Nikolaj.(none)>
2013-11-30 14:35:43 +0100

    Initial add of the railsgirls-application http://guides.railsgirls.com/app, including http://guides.railsgirls.com/thumbnails/

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railsgirls::Application.config.secret_key_base = 'dac8ac1fe8a220265077b11d9ae0f3123f4d0f072e7d80e7348745a1a53b26e40703aa3187a9f8862e1b6f3a34af3efd41117f8fbe1530b13a8163fc4eadabc9'
